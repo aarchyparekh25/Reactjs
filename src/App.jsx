c2392e591f24b101d55f005192ab60b068caf026
Aarchy Parekh <aarchy.s237544201@vcet.edu.in>
2024-07-09 22:35:57 +0530

    Added three simple text boxes for name,email and password

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>REGISTER!</h2>
+          <form action ="">
+            <div>
+              <input className='' placeholder='' type = "text"/>
+            </div>
+            <div>
+              <input className='' placeholder='' type = "email" />
+            </div>
+            <div>
+              <input className='' placeholder='' type = "password" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
